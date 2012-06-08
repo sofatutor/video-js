73d0978af80e0582d358a323c29399498cf2ab3c
Joschka <joschka@sofatutor.com>
2012-06-08 13:43:16 +0200

    change GPL comment

--- src/_begin.js
+++ src/_begin.js
@@ -1,24 +1,16 @@
 /*!
-Video.js - HTML5 Video Player
-Version GENERATED_AT_BUILD
+sofatutor.com Player - HTML5 Video Player with Flash Fallback
+Version 1.0 (June 2012)
 
-LGPL v3 LICENSE INFO
-This file is part of Video.js. Copyright 2011 Zencoder, Inc.
+Based on Video.js 3.2.1 <http://videojs.com>.
 
-Video.js is free software: you can redistribute it and/or modify
+This file is part of sofatutor.com Player. Copyright 2012 sofatutor GmbH
+
+sofatutor.com Player is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Video.js is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Video.js.  If not, see <http://www.gnu.org/licenses/>.
+(at your option) any later version. See <http://www.gnu.org/licenses/>.
 */
 
-// Self-executing function to prevent global vars and help with minification
-;(function(window, undefined){
+(function (window, undefined) {
   var document = window.document;
--- src/_end.js
+++ src/_end.js
@@ -1,6 +1,4 @@
-
 // Expose to global
 window.VideoJS = window._V_ = VideoJS;
 
-// End self-executing function
 })(window);
